26a7d8c790ab3486fcd3b48dd2a70c82f93f82cc
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2020-09-15 15:51:53 +0300

    Add files via upload

---
+++ 17.09.2020.py
@@ -0,0 +1,8 @@
+print("Sontu Alexandra")
+print("11.01.2005")
+print("Eleva in clasa 10,,D'')
+print("Republica Moldova,municipiul Balti")
+print("alexandrasontu@gmail.com")
+print("078937110")
+print("limba romana,limba germana,limba rusa,limba engleza")
+print("strada Bulgara 72\2")
